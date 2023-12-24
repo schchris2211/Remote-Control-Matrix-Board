b250579fcc78855981f74475287b76756cd9a8aa
schchris2211 <154636450+schchris2211@users.noreply.github.com>
2023-12-24 14:50:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import displayio
 import terminalio
 import time
 import supervisor
+import storage
 
 import adafruit_requests as requests
 
@@ -16,26 +17,38 @@ from os import getenv
 
 
 def check_for_updates(info_url):
-    response = requests.get(info_url, stream=True)
+    response = requests.get(info_url)
     update_info = {
         'update_code': False,
         'update_libs': False
     }
-    for chunk in response.iter_content(chunk_size=128):
-        # Process each chunk as it's downloaded
-        chunk_text = chunk.decode("utf-8")
-        if 'update_code = True' in chunk_text:
-            update_info['update_code'] = True
-        if 'update_libs = True' in chunk_text:
-            update_info['update_libs'] = True
+
+    # Process the response as a whole
+    if 'update_code = True' in response.text:
+        update_info['update_code'] = True
+    if 'update_libs = True' in response.text:
+        update_info['update_libs'] = True
 
     return update_info
 
-def download_new_run_file(url):
-    response = requests.get(url)
-    with open('/run_file.py', 'w') as file:
-        file.write(response.text)
+def download_and_save_runfile(url, filename):
+    try:
+        storage.remount("/", readonly=False)  # Remount filesystem as writable to change display.py file
+        response = requests.get(url)
+        with open(filename, "w") as file:
+            file.write(response.text)
+    finally:
+        storage.remount("/", readonly=True)  # Important: remount as read-only after the operation
 
+
+
+def report_error_to_server(error_message):
+    flask_server_url = 'http://192.168.178.43:5000'
+    try:
+        response = requests.post(flask_server_url + '/report_error', data=error_message)
+        print('Response from server:', response.text)
+    except Exception as e:
+        print('Failed to send error message:', e)
 
 
 gc.collect()
@@ -43,39 +56,48 @@ gc.collect()
 #Initiate board
 matrixportal = MatrixPortal(status_neopixel=board.NEOPIXEL, debug=True)
 
-
-# --- WiFi Setup ---
 try:
-    from secrets import secrets
-except ImportError:
-    print("WiFi secrets are kept in secrets.py, please add them there!")
-    raise
 
-print("Connecting to WiFi...")
-matrixportal.network.connect()
-print("Connected to WiFi!")
+    # --- WiFi Setup ---
+    try:
+        from secrets import secrets
+    except ImportError:
+        print("WiFi secrets are kept in secrets.py, please add them there!")
+        raise
 
-
-#Update links
-info_url = 'https://www.dropbox.com/scl/fi/3uruzvmz0rv0m3armj6o2/bool_update.txt?rlkey=bqp5v3el4kilil8mtn66pvh4s&dl=0'
-code_url = 'https://www.dropbox.com/scl/fi/1tiqy823tajurbnfuf48y/code.py?rlkey=e3c1w9yvzkmr84dx26yus4eqe&dl=0'
-lib_url = 'https://www.dropbox.com/scl/fo/f21iveuv69ls1ash5k51x/h?rlkey=2z8rgs8kpom92nf811z4kxn41&dl=0' 
+    print("Connecting to WiFi...")
+    matrixportal.network.connect()
+    print("Connected to WiFi!")
 
 
-# Check for updates
-print('Checking for Updates...')
-
-update_info = check_for_updates(info_url)
-
-print('Update Info: ', update_info)
+    #Update links
+    info_url = 'https://raw.githubusercontent.com/schchris2211/Remote-Control-Matrix-Board/main/update_info.txt'
+    display_file_url = 'https://raw.githubusercontent.com/schchris2211/Remote-Control-Matrix-Board/main/display.py'
+    # lib_url = 'https://www.dropbox.com/scl/fo/f21iveuv69ls1ash5k51x/h?rlkey=2z8rgs8kpom92nf811z4kxn41&dl=0' 
 
 
-if update_info['update_code']:
-    print('Downloading new run file...')
-    download_new_run_file(code_url)
+    # Check for updates
+    print('Checking for Updates...')
+
+    update_info = check_for_updates(info_url)
+
+    print('Update Info: ', update_info)
 
 
-#Execute Run File
-exec(open("/run_file.py").read())
+    if update_info['update_code']:
+        print('Downloading new display file...')
+        download_and_save_runfile(display_file_url, "/display.py")
+
+
+    #Execute Run File
+    exec(open("/display.py").read())
+
+
+except Exception as e:
+    error_message = str(e)
+    report_error_to_server(error_message)
+    raise  # Re-raise the exception to see it in the serial output
+
+
 
 
