bce577c64f8acafd608a8f676710396aa2e408d1
schchris2211 <154636450+schchris2211@users.noreply.github.com>
2023-12-24 17:42:42 +0100

    Update display.py

--- display.py
+++ display.py
@@ -1,4 +1,4 @@
-import gc
+achtimport gc
 import board
 import time
 import displayio
@@ -59,8 +59,8 @@ second_row = matrixportal.add_text(
 )
 
 
-update_text_and_color(first_row, 'Frohes', '#cf2727')  # Red
-update_text_and_color(second_row, 'Fest', '#008000')  # Green
+update_text_and_color(first_row, 'Frohe', '#cf2727')  # Red
+update_text_and_color(second_row, 'Weihnachten', '#008000')  # Green
 
 
 time_label = matrixportal.add_text(
