d5a9293a2b475780ca5e7c83d20a443aa159051b
schchris2211 <154636450+schchris2211@users.noreply.github.com>
2023-12-25 19:46:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import terminalio
 import time
 import supervisor
 import storage
+import json
+import os
 
 import adafruit_requests as requests
 
@@ -31,7 +33,8 @@ def check_for_updates(info_url):
 
     return update_info
 
-def download_and_save_runfile(url, filename):
+
+def update_display_file(url, filename):
     try:
         storage.remount("/", readonly=False)  # Remount filesystem as writable to change display.py file
         response = requests.get(url)
@@ -40,6 +43,55 @@ def download_and_save_runfile(url, filename):
     finally:
         storage.remount("/", readonly=True)  # Important: remount as read-only after the operation
 
+
+# def update_lib_list(list_url):
+#     try:
+#         response = requests.get(list_url)
+#         library_list = []
+#         file_markers = [f'file {i}' for i in range(1, 11)]  # Markers for 'file 1' to 'file 10'
+
+#         for marker in file_markers:
+#             if marker in response.text:
+#                 start = response.text.find(marker) + len(marker)
+#                 end = response.text.find('\n', start)
+#                 line = response.text[start:end].strip()
+#                 parts = line.split('\t')
+#                 if len(parts) >= 2:
+#                     library_list.append((parts[0], parts[1]))  # Append URL and file/folder type
+
+#         return library_list
+
+#     except MemoryError:
+#         print("Memory error occurred")
+#     except Exception as e:
+#         print("Error:", e)
+#         return []
+
+# def update_libraries(dir_url, lib_list):
+#     try:
+#         local_libs = os.listdir("/lib")
+#         print('Lib List: \n', local_libs)
+
+#         github_list = []
+#         response = requests.get(url)
+#         print(response.text)
+#         files = json.loads(response.text)
+
+#         for file in files:
+#             lib_type = 'folder' if file['type'] == 'dir' else 'file'
+#             lib_name = file['name']
+
+#             if lib_type == 'file' and lib_name.endswith('.mpy'):
+#                 lib_type = 'mpy'
+
+#             github_list.append((lib_name, lib_type))
+
+#         print('GitHub Lib List: \n', github_list)
+
+#     except MemoryError:
+#         print("Memory error occurred")
+#     except Exception as e:
+#         print("Error:", e)
 
 
 def report_error_to_server(error_message):
@@ -54,7 +106,7 @@ def report_error_to_server(error_message):
 gc.collect()
 
 #Initiate board
-matrixportal = MatrixPortal(status_neopixel=board.NEOPIXEL, debug=True)
+matrixportal = MatrixPortal(status_neopixel=board.NEOPIXEL, debug=True, rotation = 180)
 
 try:
 
@@ -73,8 +125,8 @@ try:
     #Update links
     info_url = 'https://raw.githubusercontent.com/schchris2211/Remote-Control-Matrix-Board/main/update_info.txt'
     display_file_url = 'https://raw.githubusercontent.com/schchris2211/Remote-Control-Matrix-Board/main/display.py'
-    # lib_url = 'https://www.dropbox.com/scl/fo/f21iveuv69ls1ash5k51x/h?rlkey=2z8rgs8kpom92nf811z4kxn41&dl=0' 
-
+    lib_dir_url = 'https://github.com/schchris2211/Remote-Control-Matrix-Board/tree/c5a181d02b93a7c747b0e9216c5a1f5a7f19249e/lib_update' 
+    lib_list_url = 'https://github.com/schchris2211/Remote-Control-Matrix-Board/blob/ce999c40f029f7b8cf340adef67bcaaad202bcac/update_lib_list.txt'
 
     # Check for updates
     print('Checking for Updates...')
@@ -85,9 +137,14 @@ try:
 
 
     if update_info['update_code']:
-        print('Downloading new display file...')
-        download_and_save_runfile(display_file_url, "/display.py")
+        print('Updating display file...')
+        update_display_file(display_file_url, "/display.py")
 
+    # if update_info['update_libs']:
+    #     print('Updating libraries...')
+    #     update_liblist = update_lib_list(lib_list_url)
+    #     print(update_liblist)
+    #     update_libraries(lib_url, update_liblist)
 
     #Execute Run File
     exec(open("/display.py").read())
